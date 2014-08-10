ba2f6ac98d2ea98d9873d1bff71b3fbd64adda71
Jeremy Jacobson <jjacobson93@gmail.com>
2014-08-09 22:59:03 -0700

    Added comments. Wrapped the functions to work with Node.js or a browser.

--- short.js
+++ short.js
@@ -1,148 +1,318 @@
-function abs(x) {
-	return Math.abs(x);
-}
+(function(exports) {
 
-function all(iterable) {
-	for (var i = 0; i < iterable.length(); i++) {
-		if (!iterable[i])
-			return false;
-	}
-	return true;
-}
-
-function bin(i) {
-	return i.toString(2);
-}
-
-function bool(x) {
-	return !!x;
-}
-
-function chr(i) {
-	return String.fromCharCode(i);
-}
-
-function deepClone(o) {
-	return JSON.parse(JSON.stringify(o));
-}
-
-function dir(o) {
-	return Object.keys(o);
-}
-
-function float(x) {
-	return parseFloat(x);
-}
-
-function hex(x) {
-	return x.toString(16);
-}
-
-function int(x) {
-	return parseInt(x);
-}
-
-function isinstance(o, i) {
-	return o instanceof i;
-}
-
-function len(o) {
-	return o.length;
-}
-
-function map(f, o) {
-	return o.map(f);
-}
-
-function max() {
-	if (arguments.length == 1) {
-		if (arguments[0] instanceof Array)
-			arguments = arguments[0];
+	/**
+	 * Absolute value
+	 *
+	 * @param {Number} x
+	 * @return {Number} absolute value of x
+	 */
+	exports.abs = function(x) {
+		return Math.abs(x);
 	}
 
-	var currMax = arguments[0];
-	for (var i = arguments.length - 1; i >= 0; i--) {
-		if (arguments[i] > currMax) {
-			currMax = arguments[i];
+	/**
+	 * Returns true if all elements of iterable are true
+	 *
+	 * @param {String|Array} iterable
+	 * @return {Boolean}
+	 */
+	exports.all = function(iterable) {
+		for (var i = 0; i < iterable.length; i++) {
+			if (!iterable[i]) return false;
+		};
+		return true;
+	}
+
+	/**
+	 * Returns true if any elements of iterable are true
+	 *
+	 * @param {String|Array} iterable
+	 * @return {Boolean}
+	 */
+	exports.any = function(iterable) {
+		for (var i = 0; i < iterable.length; i++) {
+			if (iterable[i]) return true;
+		};
+		return false;
+	}
+
+	/**
+	 * Return a binary string 
+	 * 
+	 * @param {Number} i
+	 * @return {String} binary string
+	 */
+	exports.bin = function(i) {
+		return i.toString(2);
+	}
+
+	/**
+	 * Convert value to a Boolean
+	 *
+	 * @param {*} [x]
+	 * @return {Boolean}
+	 */
+	exports.bool = function(x) {
+		return !!x;
+	}
+
+	/**
+	 * Return a string of one character from the character code
+	 * 
+	 * @param {Number} i
+	 * @return {String} a character
+	 */
+	exports.chr = function(i) {
+		return String.fromCharCode(i);
+	}
+
+	/**
+	 * Deep clones an object
+	 *
+	 * @param {Object} o
+	 * @return {Object} clone
+	 */
+	exports.clone = function(o) {
+		return JSON.parse(JSON.stringify(o));
+	}
+
+	/**
+	 * Returns the keys of an object. If no object is
+	 * given, then it returns the keys of window/exports
+	 * 
+	 * @param {Object} [o]
+	 * @return {Array} array of keys
+	 */
+	exports.dir = function(o) {
+		if (arguments.length == 1 && o != undefined) return Object.keys(o);
+		if (arguments.length == 1 && o == undefined) return [];
+		else return Object.keys((exports != undefined) ? exports : window);
+	}
+
+	/**
+	 * Converts a string to a float
+	 *
+	 * @param {String} [x]
+	 * @return {Number} float
+	 */
+	exports.float = function(x) {
+		return (x != undefined) ? parseFloat(x) : 0.0;
+	}
+
+	/**
+	 * Converts an integer to a hexadecimal string
+	 *	
+	 * @param {Number} x
+	 * @return {String} hexadecimal string
+	 */
+	exports.hex = function(x) {
+		return x.toString(16);
+	}
+
+	/**
+	 * Converts a string to an integer.
+	 * Returns 0 if x is undefined.
+	 * 
+	 * @param {String} [x]
+	 * @param {Number} [base]
+	 * @return {Number} integer 
+	 */
+	exports.int = function(x, base) {
+		return (x != undefined) ? parseInt(x, (base != undefined) ? base : 10) : 0;
+	}
+
+	/**
+	 * Returns true if o is an instance of i
+	 * 
+	 * @param {Object} o
+	 * @return {Boolean}
+	 */
+	exports.isinstance = function(o, i) {
+		return o instanceof i;
+	}
+
+	/**
+	 * Returns the length of a string/array
+	 *
+	 * @param {String|Array} o
+	 * @return {Number} length
+	 */
+	exports.len = function(o) {
+		return o.length;
+	}
+
+	/**
+	 * Maps an Array or Object
+	 * 
+	 * @param {Function} f
+	 * @param {Object|Array} o
+	 * @param {Object|Array} new array/object
+	 */
+	exports.map = function(f, o) {
+		return o.map(f);
+	}
+
+	/**
+	 * Returns the max of an Array of numbers or of the arguments
+	 *
+	 * @param {...Number|Number[]} x
+	 * @return {Number} max value
+	 */
+	exports.max = function(x) {
+		if (arguments.length == 1) {
+			if (arguments[0] instanceof Array)
+				arguments = arguments[0];
 		}
-	};
-	return currMax;
-}
-
-function min() {
-	if (arguments.length == 1) {
-		if (arguments[0] instanceof Array)
-			arguments = arguments[0];
+		return Math.max.apply(Math.max, arguments)
 	}
 
-	var currMin = arguments[0];
-	for (var i = arguments.length - 1; i >= 0; i--) {
-		if (arguments[i] < currMin) {
-			currMin = arguments[i];
+	/**
+	 * Returns the min of an Array of numbers or of the arguments
+	 *
+	 * @param {...Number|Number[]} x
+	 * @return {Number} min value
+	 */
+	exports.min = function(x) {
+		if (arguments.length == 1) {
+			if (arguments[0] instanceof Array)
+				arguments = arguments[0];
 		}
-	};
-	return currMin;
-}
 
-function oct(x) {
-	return x.toString(8);
-}
-
-function ord(c) {
-	return c.charCodeAt();
-}
-
-function pow(x, y, z) {
-	if (z == undefined) return Math.pow(x,y);
-	else return pow(x, y) % z;
-}
-
-function print() {
-	return console.log.apply(arguments);
-}
-
-function range(start, stop, step){
-    if (typeof stop=='undefined'){
-        // one param defined
-        stop = start;
-        start = 0;
-    };
-    if (typeof step=='undefined'){
-        step = 1;
-    };
-    if ((step>0 && start>=stop) || (step<0 && start<=stop)){
-        return [];
-    };
-    var result = [];
-    for (var i=start; step>0 ? i<stop : i>stop; i+=step){
-        result.push(i);
-    };
-    return result;
-}
-
-function reversed(seq) {
-	if (seq instanceof Array) {
-		return seq.reverse();
+		return Math.min.apply(Math.min, arguments);
 	}
 
-	if (seq instanceof String) {
-		return seq.split('').reverse().join('');
+	/**
+	 * Converts a Number to an octal string
+	 *
+	 * @param {Number} x
+	 * @return {String} octal string
+	 */
+	exports.oct = function(x) {
+		return x.toString(8);
 	}
-}
 
-function round(x, n) {
-	return parseFloat(x.toFixed((n != undefined) ? n : 0));
-}
+	/**
+	 * Given a character, return the Unicode number representation
+	 *
+	 * @param {String} c
+	 * @return {Number} unicode number
+	 */
+	exports.ord = function(c) {
+		return c.charCodeAt();
+	}
 
-function sum(seq, start) {
-	return seq.slice(start).reduce(function(a, b) { return a + b });
-}
+	/**
+	 * Return x to the power of y.
+	 * If z is present, return x to the power y, modulo z
+	 *
+	 * @param {Number} x
+	 * @param {Number} y
+	 * @param {Number} [z]
+	 * @return {Number}
+	 */
+	exports.pow = function(x, y, z) {
+		if (z == undefined) return Math.pow(x,y);
+		else return pow(x, y) % z;
+	}
 
-function zip(x, y) {
-	var n = [];
-	var l = (x.length > y.length) ? y.length : x.length;
-	for (var i = l - 1; i >= 0; i--) {
-		n.push([x[i], y[i]]);	
-	};
-	return n;
-}
+	/**
+	 * Prints to the console (alias of console.log)
+	 *
+	 * @param {...*} [arguments]
+	 * @return {undefined}
+	 */
+	exports.print = function() {
+		return console.log.apply(console, arguments);
+	}
+
+	/**
+	 * Returns a range from start to stop with step steps.
+	 * If one argument is given, stop = start.
+	 *
+	 * @param {Number} start=0
+	 * @param {Number} [stop=start]
+	 * @param {Number} [step]
+	 * @return {Number[]}
+	 */
+	exports.range = function(start, stop, step){
+	    if (typeof stop=='undefined'){
+	        // one param defined
+	        stop = start;
+	        start = 0;
+	    };
+	    if (typeof step=='undefined'){
+	        step = 1;
+	    };
+	    if ((step>0 && start>=stop) || (step<0 && start<=stop)){
+	        return [];
+	    };
+	    var result = [];
+	    for (var i=start; step>0 ? i<stop : i>stop; i+=step){
+	        result.push(i);
+	    };
+	    return result;
+	}
+
+	/**
+	 * Returns the reverse of an Array or String
+	 * 
+	 * @param {Array|String} seq
+	 * @return {Array|String} reversed
+	 */
+	exports.reversed = function(seq) {
+		if (seq instanceof Array) {
+			return seq.reverse();
+		}
+
+		if (typeof(seq) == 'string' || seq instanceof String) {
+			return seq.split('').reverse().join('');
+		}
+	}
+
+	/**
+	 * Rounds x to n digits. If n is undefined, n = 0.
+	 *	
+	 * @param {Number} x
+	 * @param {Number} [n=0]
+	 * @return {Number} rounded number
+	 */
+	exports.round = function(x, n) {
+		return parseFloat(x.toFixed((n != undefined) ? n : 0));
+	}
+
+	/**
+	 * Sums all the elements of seq. If start is given,
+	 * the sum starts there.
+	 *
+	 * @param {Number[]} seq
+	 * @param {Number} start
+	 * @return {Number} sum
+	 */
+	exports.sum = function(seq, start) {
+		return seq.slice(start).reduce(function(a, b) { return a + b });
+	}
+
+	/**
+	 * Returns a Array of (arguments.length)-sized Arrays,
+	 * where the i-th element contains the i-th element
+	 * from each sequence. The returned Array is truncated
+	 * in length to the length of the shortest argument sequence
+	 * 
+	 * @param {...Array} [arguments]
+	 * @return {Array} zipped 
+	 */
+	exports.zip = function() {
+		var args = [].slice.call(arguments);
+		var n = [];
+		var s = (args.length == 0) ? [] : args.reduce(function(a,b) {
+	        return (a.length < b.length) ? a : b
+	    });
+
+		for (var i = 0; i < s.length; i++) {
+			n.push(args.map(function(a){return a[i]}));
+		};
+
+		return n;
+	}
+
+})(typeof exports === 'undefined' ? this : exports);
